881a20638172da4134d1b132d1759f957804b63b
marusiakulyk <33452986+marusiakulyk@users.noreply.github.com>
2018-02-25 13:55:51 +0200

    Initial commit

---
+++ pyfacebook-master/examples/fbsample/fbapp/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('fbsample.fbapp.views',
+                       (r'^$', 'canvas'),
+                       # Define other pages you want to create here
+                       )
