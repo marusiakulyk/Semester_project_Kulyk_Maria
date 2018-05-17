27c0b5d0456f49c4d7a6219d51601710d3ff31e4
marusiakulyk <33452986+marusiakulyk@users.noreply.github.com>
2018-05-17 14:36:33 +0300

    update in forecastio info

--- examples/forecast_class.py
+++ examples/forecast_class.py
@@ -2,7 +2,7 @@ class Forecast:
     def __init__(self):
         self.info = {}
 
-    def add(self, json,  path, key):
+    def add(self, json, path, key):
         a = json
         for i in path:
             a = a[i]
@@ -12,6 +12,7 @@ class Forecast:
             self.info[key] += [a]
 
     def close(self):
+        # can be used oly if all info are floats
         for i in self.info:
             self.info[i] = sum(self.info[i])/len(self.info[i])
 
